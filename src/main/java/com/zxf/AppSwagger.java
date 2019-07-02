a987cf5a57234e71066a1fc386d033d99aed77e2
zhaoxiaofeng <1278442828@qq.com>
2019-07-02 19:46:44 +0800

    第一次提交

---
+++ src/main/java/com/zxf/AppSwagger.java
@@ -0,0 +1,15 @@
+package com.zxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@SpringBootApplication
+@EnableSwagger2//开启swagger2
+public class AppSwagger {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSwagger.class, args);
+	}
+
+}
---
+++ src/main/java/com/zxf/controller/UploadController.java
@@ -0,0 +1,101 @@
+package com.zxf.controller;
+
+import java.io.File;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+public class UploadController {
+	@Value("${file.path}")
+	private String realPath;
+	
+	
+	
+	/**
+	 * 上传单个文件
+	 * @param uploadFile
+	 * @param request
+	 * @return
+	*/
+	@ApiOperation(value = "一次性上传单个文件")
+	@PostMapping("/upload")
+	public String upload(MultipartFile uploadFile,HttpServletRequest request) {
+//		String realPath = request.getSession().getServletContext().getRealPath("/uploadFile");
+		String realPath = "E:/uploadFile";
+		System.out.println(request.getSession().getServletContext().toString());
+		File dir = new File(realPath);
+		if(!dir.isDirectory()) {//文件目录不存在
+			dir.mkdirs();//创建文件夹
+		}
+		try {
+			//文件名
+			String fileName = uploadFile.getOriginalFilename();
+			//服务端保存的文件对象
+			File fileServer = new File(dir,fileName);
+			System.out.println("file文件真实路劲"+fileServer.getAbsolutePath());
+			//实现上传
+			uploadFile.transferTo(fileServer);
+			String filePath = request.getScheme()+"://"+request.getServerName()
+			+":"+request.getServerPort()+"/uploadFile/"+fileName;
+			return filePath; 
+		} catch (IllegalStateException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "上传失败！";
+	}
+	/**
+	 * 上传多个文件
+	 * @param uploadFiles
+	 * @param request
+	 * @return
+	*/
+	@ApiOperation(value = "一次性上传多个文件")
+	@PostMapping("/uploads")
+	public String uploads(MultipartFile[] uploadFiles,HttpServletRequest request) {
+		//1、对文件数组做判空
+		if(uploadFiles.length<1 || uploadFiles==null) {
+			
+			return "文件不能为空！！！";
+		}
+		//2、定义文件的存储路径
+		//String realPath = request.getSession().getServletContext().getRealPath("/uploadFile/");
+		//String realPath = "E:/uploadFile/";
+		//System.out.println(request.getServletContext().getRealPath("")+"/WEB-INF/classes/static/");
+		File dir = new File(realPath);
+		if(!dir.isDirectory()) {
+			dir.mkdirs();
+		}
+		try {
+			//3、遍历文件数组，一个个上传
+			for(int i=0;i<uploadFiles.length;i++) {
+				MultipartFile uploadFile = uploadFiles[i];
+				String fileName = uploadFile.getOriginalFilename();
+				//服务端保存文件对象
+				File fileServer = new File(dir, fileName);
+				System.out.println("file文件真实路劲"+fileServer.getAbsolutePath());
+				//4、实现上传
+				uploadFile.transferTo(fileServer);
+				//http://127.0.0.1:8091/uploadFile/2.JPG
+				String filePath = request.getScheme()+"://"+request.getServerName()
+								  +":"+request.getServerPort()+"/uploadFile/"+fileName;
+			}
+			return "上传成功！上传文件至"+realPath;
+		} catch (IllegalStateException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "上传失败！";
+		
+	}
+
+}
